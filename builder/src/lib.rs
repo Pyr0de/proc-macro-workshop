66420542380e09ed037ebc81594f87e9f65dd5da
Pyrode <pyrode.01@gmail.com>
2025-04-14 13:02:13 +0530

    04-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,58 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    let builder_name = format!("{}Builder", ident);
+    let builder_ident = Ident::new(&builder_name, ident.span());
+    
+    quote! {
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
 
-    unimplemented!()
+        impl #builder_ident {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+            pub fn build(&mut self) -> Option<#ident> {
+                Some(#ident {
+                    executable: self.executable.clone().unwrap_or(String::new()),
+                    args: self.args.clone().unwrap_or(vec![]),
+                    env: self.env.clone().unwrap_or(vec![]),
+                    current_dir: self.current_dir.clone().unwrap_or(String::new()),
+                })
+            }
+        }
+
+        impl #ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    }.into()
 }
